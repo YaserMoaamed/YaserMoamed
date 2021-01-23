2b4d2c38c6d37b2279fc8ce06bf33b556c9b717d
YaserMoaamed <yasermoaameed@gmail.com>
2021-01-23 06:24:06 +0100

    created a header of the portofolio and footer folder

---
+++ src/portofolio.js
@@ -0,0 +1,14 @@
+const settings = {
+  isSplash: false,
+};
+
+//Home Page
+const greeting = {
+  title: "Yaser Mohamed",
+  logo_name: "yasermoamed",
+  nickname: "Yash",
+  subTitle: "Hi, I'm Yaser Mohamed, a Full Stack Developer 🚀 from Milan",
+  resumeLink: " ",
+  portfolio_repository: "https://github.com/YaserMoaamed/yasermoamed",
+};
+export { settings, greeting };
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export const blueTheme = {
+  body: "#EDF9FE",
+  text: "#001C55",
+  highlight: "#A6E1FA",
+  dark: "#00072D",
+  secondaryText: "#7F8DAA",
+  imageHighlight: "#0E6BA8",
+  compImgHighlight: "#E6E6E6",
+  jacketColor: "#0A2472",
+  headerColor: "#0E6BA877",
+};
+
+export const blackTheme = {
+  body: "#E5E5E5",
+  text: "#14213d",
+  highlight: "#ffffff",
+  dark: "#000000",
+  secondaryText: "#5A6377",
+  imageHighlight: "#fca311",
+  compImgHighlight: "#E6E6E6",
+  jacketColor: "#8d99ae",
+  headerColor: "#fca31177",
+};
+
+export const chosenTheme = blackTheme;
